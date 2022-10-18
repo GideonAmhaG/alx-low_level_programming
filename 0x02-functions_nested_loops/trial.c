d78b9be28a74e3af002ee8758ba731d7fbeab81e
GideonAmhaG <gideonamaha@gmail.com>
2022-10-18 13:56:39 +0000

    7-2

--- 0x02-functions_nested_loops/trial.c
+++ 0x02-functions_nested_loops/trial.c
@@ -13,12 +13,6 @@ int print_last_digit(int n)
 		_putchar((-n % 10) + '0');
 		return (-n % 10);
 	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-
 	else
 	{
 		_putchar((n % 10) + '0');
