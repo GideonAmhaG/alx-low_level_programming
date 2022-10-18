ebeabd890b06fd6bccc503918da277d4c246cce3
GideonAmhaG <gideonamaha@gmail.com>
2022-10-18 11:35:18 +0000

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints _putchar
+ *
+ *Return: void
+ */
+
+void print_alphabet(void)
+{
+	char lt;
+
+	for(lt = 'a'; lt <= 'z'; lt++)
+		_putchar(lt);
+	
+	_putchar('\n');
+
+	return;
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,8 +3,10 @@
 
 /*
  * File : main.h
+ * Functions for c loops and functions
  */
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
