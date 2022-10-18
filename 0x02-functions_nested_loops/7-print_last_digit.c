6074dce39f07824f7b86b4dc8477783ae3fd26ac
GideonAmhaG <gideonamaha@gmail.com>
2022-10-18 14:03:41 +0000

    7-3

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,21 +8,13 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-	{
-		_putchar((-n % 10) + '0');
-		return (-n % 10);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
+	int m = n % 10;
 
-	else
-	{
-		_putchar((n % 10) + '0');
-		return (n % 10);
-	}
+	if (m < 0)
+		m *= -1;
+
+	_putchar(m + '0');
+
+	return (m);
 }
 
