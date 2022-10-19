67e716914545051b0ba2b11ba5380ee9244f54b9
GideonAmhaG <gideonamaha@gmail.com>
2022-10-19 13:40:58 +0000

    0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File : main.h
+ * Functions for debugging
+ */
+
+void positive_or_negative(int);
+
+#endif
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Determines if a random number is positive, negative or zero
+*
+* Return: is zero
+*/
+
+void positive_or_negative(int n)
+{
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+}
