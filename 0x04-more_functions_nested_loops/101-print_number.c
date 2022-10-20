9b2b6f8f5a16854aa5574bad0658dab1c0f8281f
GideonAmhaG <gideonamaha@gmail.com>
2022-10-20 14:43:21 +0000

    12-2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,15 +8,18 @@
 
 void print_number(int n)
 {
+	unsigned int m = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
+		m = n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (m / 10)
+		print_number(m / 10);
 
-	_putchar(n % 10 + '0');
+	_putchar((unsigned int) n % 10 + '0');
 }
 
