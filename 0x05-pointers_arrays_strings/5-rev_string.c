eb4fc3752eca404ee839bad757969f2397c397a0
GideonAmhaG <gideonamaha@gmail.com>
2022-10-25 11:02:07 +0000

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * rev_string - reverese a string
+  * @s: input string
+  *
+  * Return: void
+  */
+
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	char k;
+
+	while (s[i++])
+		j++;
+	
+	for (i = j - 1; i >= j / 2; i--)
+	{
+		k = s[i];
+		s[i] = s[j - 1 - i];
+		s[j - 1 - i] = k;			
+	}
+
+}
