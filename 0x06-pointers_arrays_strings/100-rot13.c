3d12198410e2ffeea3defc4f60e4264217650702
GideonAmhaG <gideonamaha@gmail.com>
2022-10-27 11:57:22 +0000

    100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,16 +9,23 @@
 
 char *rot13(char *s)
 {
-	int i = 0; 
+	int i = 0;
 
 	while (s[i])
 	{
-		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <=
-					'M'))
-			s[i] += 13;
-		else if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i]
+		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <=
+					'Z'))
+		{
+			if ((s[i] > 'm' && s[i] <= 'z') || (s[i] > 'M' && s[i]
 					<= 'Z'))
-			s[i] -= 13;
+			{
+				s[i] -= 13;
+				break;
+			}
+
+			s[i] += 13;
+			break;
+		}
 
 		i++;
 	}
