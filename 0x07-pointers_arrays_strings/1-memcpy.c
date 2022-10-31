158bb8dd0a9688da76ff86d3edd0628fa3bd371c
GideonAmhaG <gideonamaha@gmail.com>
2022-10-31 12:58:33 +0000

    1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+  * _memcpy - copies memory
+  * @dest: to be copied to
+  * @src: to be copied from
+  * @n: how much to copy
+  *
+  * Return: pointer to modified buffer
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
