1a426c064818f572a764ae3bae6476e50adcba1d
GideonAmhaG <gideonamaha@gmail.com>
2022-11-01 12:42:31 +0000

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+/**
+  * _strstr - finds the first occurrence of the substring needle in the
+  * string haystack
+  * @haystack: string to be checked
+  * @needle: substring
+  *
+  * Return: a pointer to the beginning of the substring, or NULL if the
+  * substring is not found
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j;
+
+	while (haystack[i])
+	{
+		j = 0;
+
+		while (haystack[i] == needle[j])
+		{
+			return (haystack + i);
+
+			j++;
+		}
+
+		i++;
+	}
+
+	return ('\0');
+}
