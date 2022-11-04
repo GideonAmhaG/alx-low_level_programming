5491be5e4db8d0c472a893b58d118b5b0dcafa22
GideonAmhaG <gideonamaha@gmail.com>
2022-11-04 09:58:22 +0000

    2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,11 +12,8 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-			printf("%s\n", argv[i]);
-	}
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 
 	return (0);
 }
