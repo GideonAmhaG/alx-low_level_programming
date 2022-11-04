2ef44c4fbd1af8fc0ca0b90e4d4016f220833af3
GideonAmhaG <gideonamaha@gmail.com>
2022-11-04 09:53:58 +0000

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - prints all arguments on a new line
+  * @argc: number of arguments
+  * @argv: array of the strings of arguments
+  *
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
+
