d3de511fe21f53b94e0c5fc44fa046d6b0b95df4
GideonAmhaG <gideonamaha@gmail.com>
2022-11-08 15:40:36 +0000

    4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,7 +4,7 @@
 /**
   * alloc_grid - returns a pointer to a 2d array of integers
   * @width: width of array
-  * @height: length of array
+  * @height: height of array
   *
   * Return: pointer to the array
   */
