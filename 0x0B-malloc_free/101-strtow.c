f1eb53f952b739f6f292ae9d711dbc184a785468
GideonAmhaG <gideonamaha@gmail.com>
2022-11-08 19:55:26 +0000

    6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * strtow - splits a string into words
+  * @str: string to be split
+  *
+  * Return: pointer to an array of split words
+  */
+
+char **strtow(char *str)
+{
+	int i, j, l = 0;
+	char **s;
+
+	if (str == NULL || str == "")
+		return (NULL);
+}
