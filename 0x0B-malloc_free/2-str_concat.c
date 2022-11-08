1e9fbe72a7a40aa50df492a7657feea4aa34bcc1
GideonAmhaG <gideonamaha@gmail.com>
2022-11-08 14:17:05 +0000

    2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * str_concat - joins 2 strings
+  * @s1: 1st string
+  * @s2: 2nd string
+  *
+  * Return: pointer to the array
+  */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j = 0, l = 0;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] || s2[i]; i++)
+		l++;
+
+	s = malloc(sizeof(char) * l);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		s[j++] = s1[i];
+
+	for (i = 0; s2[i]; i++)
+		s[j++] = s2[i];
+
+	s[j] == '\0';
+
+	return (s);
+}
