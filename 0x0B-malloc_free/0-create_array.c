73e24fed6b06ef1aef1f781d55af9e4abdefc6dc
GideonAmhaG <gideonamaha@gmail.com>
2022-11-08 10:17:35 +0000

    0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * create_array - creates an array of char
+  * @size: sds
+  * @c: sfdfs
+  * Return: void
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	return (malloc(sizeof(c) * size));
+}
