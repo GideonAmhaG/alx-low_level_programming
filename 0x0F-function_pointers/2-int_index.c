e4f5c0ba306ec5a1b35519fd6c80e1dd6db10364
GideonAmhaG <gideonamaha@gmail.com>
2022-11-16 13:41:07 +0000

    0

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,17 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array != NULL && cmp != NULL)
-	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]))
-				break;
-			else if (i == size - 1)
-				return (-1);
-		}
+	if (array == NULL && cmp == NULL)
+		return (-1);
 
-		return (i);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
 	}
 
 	return (-1);
