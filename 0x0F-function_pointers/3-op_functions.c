22ad45cf8a35a10a529fdfb9200564ae69609551
GideonAmhaG <gideonamaha@gmail.com>
2022-11-17 11:22:35 +0000

    3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ *op_add - adds 2 numbers
+ *@a: num1
+ *@b: num2
+ *
+ *Return: add
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - subtracts 2 numbers
+ *@a: num1
+ *@b: num2
+ *
+ *Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - multiplies 2 numbers
+ *@a: num1
+ *@b: num2
+ *
+ *Return: mult
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - divides 2 numbers
+ *@a: num1
+ *@b: num2
+ *
+ *Return: division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - mods 2 numbers
+ *@a: num1
+ *@b: num2
+ *
+ *Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
