6b465c7878df60d8e6c87f762ca7f2d615bebd4b
GideonAmhaG <gideonamaha@gmail.com>
2022-11-17 11:55:41 +0000

    4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,3 +8,41 @@
  *
  * Return: 0.
  */
+
+int main(int argc, char *argv[])
+{
+	int b, i;
+	unsigned char o;
+	int (*a)(int, char**) = main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	b = atoi(argv[1]);
+
+	if (b < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < b; i++)
+	{
+		o = *(unsigned char *)a;
+		printf("%.2x", o);
+
+		if (i == b - 1)
+			continue;
+
+		printf(" ");
+
+		a++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
