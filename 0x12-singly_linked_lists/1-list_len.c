d68f0d5811ce77b3de5f3cf0a3f30daa967cea74
GideonAmhaG <gideonamaha@gmail.com>
2022-11-24 17:36:38 +0000

    1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of nodes in a linked list
+ * @h: pointer to head node
+ *
+ * Return: the number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
