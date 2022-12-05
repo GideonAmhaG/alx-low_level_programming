cee2174f7dcefb0e9ed49b17dc298d6948ae5813
GideonAmhaG <gideonamaha@gmail.com>
2022-12-05 13:34:08 +0000

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,8 +15,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[len])
-		len++;
+	if (!text_context)
+	{
+		while (text_content[len])
+			len++;
+	}
 
 	fd_o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	fd_w = write(fd_o, text_content, len);
