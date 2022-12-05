449434b696784c3ef59da76d7cedeb1f03427435
GideonAmhaG <gideonamaha@gmail.com>
2022-12-05 13:31:35 +0000

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to be created
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd_o, fd_w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	while (text_content[len])
+		len++;
+
+	fd_o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd_w = write(fd_o, text_content, len);
+
+	if (fd_o == -1 || fd_w == -1)
+		return (-1);
+
+	close(fd_o);
+
+	return (1);
+}
