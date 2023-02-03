6a4ed0ca551e26b9a4a6f3ba86a4b9a32f3ba834
GideonAmhaG <gideonamha@gmail.com>
2023-02-03 12:47:12 +0300

    removed elem var

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx = 0;
-	hash_node_t *elem, *new;
+	hash_node_t *new;
 
 	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (0);
